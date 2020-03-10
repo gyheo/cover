92698dc05fd26414728a2390b35b198072bfc8b7
gyheo <heo.geoyeo@gmail.com>
2020-03-11 01:23:59 +0900

    Edit a condition

--- jsGame/jsWord.js
+++ jsGame/jsWord.js
@@ -1,4 +1,8 @@
 const body = document.body; // Document Object Model 이해
+
+const gameTitle = document.createElement('h1');
+gameTitle.textContent = '쿵쿵따!'
+
 let word = document.createElement('div');
 word.textContent = '한글날';
 
@@ -16,16 +20,53 @@ document.body.append(result);
 document.body.append(submitButton);
 
 // 영상 보기 전
-submitButton.onclick = function () {
-    if(word.textContent[word.textContent.length - 1] == answer.value[0]) {
-        alert('GOOD!');
-        word.textContent = answer.value;
-        answer.value = '';
-    } else if (answer.value[0] == ' ') { // 공백 처리
-        alert('No Space!');
-        answer.value = '';
-    } else { // 오답 처리
-        alert('TRY AGAIN!');
+// submitButton.onclick = function () {
+//     let answerLength = answer.value.length; 
+
+//     if(answerLength > 1 && answerLength <= 3) { // 글자 수 제한
+//         if(word.textContent[word.textContent.length - 1] === answer.value[0]) {
+//             if(answer.value.includes(' ')){ 
+//                 // 공백 처리
+//                 result.textContent = '빈칸 입력 ❌';
+//                 answer.value = '';
+//                 return;
+//             }
+    
+//             result.textContent = '정답 ⭕️!';
+//             word.textContent = answer.value;
+//             answer.value = '';
+//         } else { // 오답 처리
+//             result.textContent = '오답 ❌!';
+//             answer.value = '';
+//         }
+//     } else {
+//         result.textContent = '최소 2글자, 최대 3글자!';
+//         answer.value = '';
+//     }
+// }
+
+// 수업
+submitButton.addEventListener('click', function () {
+    let answerLength = answer.value.length; 
+
+    if(answerLength > 1 && answerLength <= 3) { // 글자 수 제한
+        if(word.textContent[word.textContent.length - 1] === answer.value[0]) {
+            if(answer.value.includes(' ')){ 
+                // 공백 처리
+                result.textContent = '빈칸 입력 ❌';
+                answer.value = '';
+                return;
+            }
+    
+            result.textContent = '정답 ⭕️!';
+            word.textContent = answer.value;
+            answer.value = '';
+        } else { // 오답 처리
+            result.textContent = '오답 ❌!';
+            answer.value = '';
+        }
+    } else {
+        result.textContent = '최소 2글자, 최대 3글자!';
         answer.value = '';
     }
-}
+});
