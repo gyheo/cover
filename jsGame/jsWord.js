9ab827e09e02c659d8b0b668276a242b12cbc70c
gyheo <heo.geoyeo@gmail.com>
2020-03-11 00:05:02 +0900

    Making a jsWord

---
+++ jsGame/jsWord.js
@@ -0,0 +1,27 @@
+const body = document.body; // Document Object Model 이해
+let word = document.createElement('div');
+word.textContent = '한글날';
+
+document.body.append(word);
+
+let answer = document.createElement('input');
+document.body.append(answer);
+
+let submitButton = document.createElement('button');
+submitButton.textContent = '입력';
+
+let result = document.createElement('div');
+document.body.append(result);
+
+document.body.append(submitButton);
+
+// 영상 보기 전
+submitButton.onclick = function () {
+    if(word.textContent[word.textContent.length - 1] == answer.value[0]) {
+        alert('GOOD!');
+        word.textContent = answer.value;
+        answer.value = '';
+    } else {
+        alert('TRY AGAIN!');
+    }
+}
