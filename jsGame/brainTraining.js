fc57a08868911ab70eb0278fabc035b1e7afb73a
gyheo <heo.geoyeo@gmail.com>
2020-03-14 15:42:49 +0900

    Making a brain training game

---
+++ jsGame/brainTraining.js
@@ -0,0 +1,44 @@
+let number1 = Math.ceil(Math.random() * 9);
+let number2 = Math.ceil(Math.random() * 9);
+let answer = number1 * number2;
+
+let question = document.createElement('div');
+document.body.append(question);
+
+let form = document.createElement('form');
+document.body.append(form);
+
+let userInput = document.createElement('input');
+userInput.type = 'number';
+form.append(userInput);
+
+let button = document.createElement('button');
+button.textContent = '입력';
+form.append(button);
+
+question.textContent = number1 + ' X ' + number2 + ' = ?';
+
+let result = document.createElement('div');
+document.body.append(result);
+
+userInput.focus();
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    if(parseInt(userInput.value) == answer) {
+        result.textContent = '정답 ⭕️';
+        
+        // 정답일 경우 새로운 문제
+        let number1 = Math.ceil(Math.random() * 9);
+        let number2 = Math.ceil(Math.random() * 9);
+
+        question.textContent = number1 + ' X ' + number2 + ' = ?';
+        answer = number1 * number2;
+    } else {
+        result.textContent = '다시!'; // 오답일 경우에는 계속 그 문제 풀도록
+    }
+
+    userInput.value = ''; // 초기화
+    userInput.focus();
+})
