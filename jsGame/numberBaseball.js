571fbf7597f331c743add559affefca056faf001
gyheo <heo.geoyeo@gmail.com>
2020-03-15 23:28:28 +0900

    Practice push, pop, shift, unshfit

---
+++ jsGame/numberBaseball.js
@@ -0,0 +1,33 @@
+const body = document.body;
+
+const gameTitle = document.createElement('h1');
+gameTitle.textContent = '숫자 야구!'
+document.body.append(gameTitle);
+
+let numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let numbers = [];
+
+for (let i = 0; i < 4; i++) {
+    // let num = numberArr.pop();
+    let num = numberArr.shift();
+    numbers.unshift(num);
+    console.log(numbers);
+}
+
+let form = document.createElement('form');
+document.body.append(form);
+
+let answer = document.createElement('input');
+form.append(answer);
+
+let submitButton = document.createElement('button');
+submitButton.textContent = '입력';
+
+let result = document.createElement('div');
+document.body.append(result);
+
+form.append(submitButton);
+
+submitButton.onclick = function (e) {
+    e.preventDefault();
+}
