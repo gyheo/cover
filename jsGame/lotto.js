b6c9dae219f0e41e194a3a3c4449cfd3f5bd6172
gyheo <heo.geoyeo@gmail.com>
2020-04-16 23:24:31 +0900

    Practice the splice and sort

--- jsGame/lotto.js
+++ jsGame/lotto.js
@@ -12,10 +12,34 @@ let numberArrays = Array(45)
                     });
 console.log(numberArrays);
 
-let lotto = [];
+let shuffle = [];
 while (numberArrays.length > 0) {
     let num = numberArrays.splice(Math.floor(Math.random() * numberArrays.length), 1)[0];
-    lotto.push(num);
+    // console.log(numberArrays.length);
+    shuffle.push(num);
 }
 
-console.log(lotto);
+console.log(shuffle);
+
+let bonus = shuffle[shuffle.length - 1];
+let lotto = shuffle
+            .splice(0, 6)
+            .sort(function (a, b) { 
+                return a - b
+            });
+
+console.log(`당첨숫자 : ${lotto}, 보너스 : ${bonus}`);
+const result = document.getElementById('result');
+
+for (let i = 0; i < lotto.length; i++) {
+    setTimeout(function timeInterval() {
+        let circle = document.createElement('div');
+        circle.textContent = lotto[i];
+        result.appendChild(circle);
+    }, 1000);
+}
+
+const luckys = document.getElementsByClassName('luckys')[0];
+let bonusCircle = document.createElement('div');
+bonusCircle.textContent = bonus;
+luckys.appendChild(bonusCircle);
