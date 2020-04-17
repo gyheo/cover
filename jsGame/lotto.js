2964c440a0eafc8b29118d069e17d94ef64149ad
gyheo <heo.geoyeo@gmail.com>
2020-04-17 23:30:03 +0900

    Making a lotto (not using a closure)

--- jsGame/lotto.js
+++ jsGame/lotto.js
@@ -31,15 +31,62 @@ let lotto = shuffle
 console.log(`당첨숫자 : ${lotto}, 보너스 : ${bonus}`);
 const result = document.getElementById('result');
 
-for (let i = 0; i < lotto.length; i++) {
-    setTimeout(function timeInterval() {
-        let circle = document.createElement('div');
-        circle.textContent = lotto[i];
-        result.appendChild(circle);
-    }, 1000);
+// for (let i = 0; i < lotto.length; i++) {
+//     setTimeout(function timeInterval() {
+//         let circle = document.createElement('div');
+//         circle.style.display = 'inline-block';
+//         circle.style.border = '1px solid black';
+//         circle.style.borderRadius = '10px';
+//         circle.style.width = '20px';
+//         circle.style.height = '20px';
+//         circle.style.textAlign = 'center';
+//         circle.style.marginRight = '10px';
+//         circle.textContent = lotto[i];
+//         result.appendChild(circle);
+//     }, 1000);
+// }
+
+function decorateBall(ballNumber, result) {
+    let circle = document.createElement('div');
+    circle.textContent = ballNumber;
+    circle.style.display = 'inline-block';
+    circle.style.border = '1px solid black';
+    circle.style.borderRadius = '10px';
+    circle.style.width = '20px';
+    circle.style.height = '20px';
+    circle.style.textAlign = 'center';
+    circle.style.marginRight = '10px';
+    result.appendChild(circle);
 }
+
+setTimeout(function timeInterval() {
+    decorateBall(lotto[0], result);
+}, 1000);
+
+setTimeout(function timeInterval() {
+    decorateBall(lotto[1], result);
+}, 2000);
+
+setTimeout(function timeInterval() {
+    decorateBall(lotto[2], result);
+}, 3000);
+
+setTimeout(function timeInterval() {
+    decorateBall(lotto[3], result);
+}, 4000);
+
+setTimeout(function timeInterval() {
+    decorateBall(lotto[4], result);
+}, 5000);
+
+setTimeout(function timeInterval() {
+    decorateBall(lotto[5], result);
+}, 6000);
+
 
 const luckys = document.getElementsByClassName('luckys')[0];
 let bonusCircle = document.createElement('div');
-bonusCircle.textContent = bonus;
-luckys.appendChild(bonusCircle);
+
+setTimeout(function timeInterval() {
+    decorateBall(bonus, luckys);
+}, 7000);
